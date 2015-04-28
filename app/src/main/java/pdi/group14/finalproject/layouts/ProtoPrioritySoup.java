dd4592953f7fa93e9446e5510ced599c0db790f0
Jeppe <jeppe.hartmund@gmail.com>
2015-04-28 14:35:46 +0200

    half-way implemented the priority soup

---
+++ app/src/main/java/pdi/group14/finalproject/layouts/ProtoPrioritySoup.java
@@ -0,0 +1,36 @@
+package pdi.group14.finalproject.layouts;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by jeppe on 28-04-15.
+ */
+public class ProtoPrioritySoup extends ViewGroup {
+    public ProtoPrioritySoup(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int x = r + (l-r)/2;
+        int y = t + (b-t)/2;
+        for(int i = 0; i< getChildCount(); i++){
+            View v = getChildAt(i);
+            if( i == 0){
+                v.layout();
+            }else if(i == 1){
+
+            }else if(i == 2){
+
+            }else if(i == 3){
+
+            }else if(i == 4){
+
+            }
+        }
+
+    }
+}
